6faaeef010105e474526a39368e76e2665a44b40
ryzen-xp <iron4540@gmail.com>
2026-05-20 11:41:01 +0530

    feat: add OpenZeppelin submodule and update Vault contract with IERC721 import

--- contracts/EVM/src/Vault.sol
+++ contracts/EVM/src/Vault.sol
@@ -1,12 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "@openzeppelin/token/ERC721/IERC721.sol";
+
 contract Vault {
+    
+    
+    //   LockNFT
 
-//   LockNFT 
+    function lockNFT() external {
 
-// 
+    }
+    
+    //  Trusted setup  is
 
-
-     
-}
+    }
--- contracts/EVM/src/token.sol
+++ contracts/EVM/src/token.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+
 contract Token {
     uint256 public number;
 
